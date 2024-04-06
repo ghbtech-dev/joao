dcb61e52bf77611f700695cb789567fad43ecf20
ghbtech-dev <55549505+ghbtech-dev@users.noreply.github.com>
2024-04-05 22:56:15 -0300

    Create exibicao.js

---
+++ exibicao.js
@@ -0,0 +1,21 @@
+// Simulação de dados para exibição na tabela
+let dados = [
+    { acompanhante: 'George', diaSemana: 'Segunda-feira', diaMes: 5, mes: 'Abril', turno: 'Manhã' },
+    { acompanhante: 'Alysson', diaSemana: 'Terça-feira', diaMes: 6, mes: 'Abril', turno: 'Tarde' },
+    // Adicione mais dados conforme necessário
+];
+
+window.onload = function() {
+    let tbody = document.getElementById('tbodyAcompanhantes');
+
+    // Adiciona os dados à tabela
+    dados.forEach(dado => {
+        let tr = document.createElement('tr');
+        tr.innerHTML = `<td>${dado.acompanhante}</td>
+                        <td>${dado.diaSemana}</td>
+                        <td>${dado.diaMes}</td>
+                        <td>${dado.mes}</td>
+                        <td>${dado.turno}</td>`;
+        tbody.appendChild(tr);
+    });
+};
